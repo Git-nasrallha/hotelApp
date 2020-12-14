a3d6dc2e787a810600ac87f363a35b7107e89757
Git-nasrallha <mohamednasrallha@gmail.com>
2020-12-13 23:10:06 -0800

    Updates

---
+++ precache-manifest.806a45a0421c3a80c1279a8ca769e42e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a360fe27fbee58055fb92d94c994818f",
+    "url": "/Git-nasrallha/hotelApp.git/index.html"
+  },
+  {
+    "revision": "3b538525b7a7983b6cef",
+    "url": "/Git-nasrallha/hotelApp.git/static/css/2.fc10f3f3.chunk.css"
+  },
+  {
+    "revision": "0c4a2047be9b9ca63546",
+    "url": "/Git-nasrallha/hotelApp.git/static/css/main.aa705513.chunk.css"
+  },
+  {
+    "revision": "3b538525b7a7983b6cef",
+    "url": "/Git-nasrallha/hotelApp.git/static/js/2.451da81f.chunk.js"
+  },
+  {
+    "revision": "7286878f6d60592af396a3340b45cb8a",
+    "url": "/Git-nasrallha/hotelApp.git/static/js/2.451da81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4a2047be9b9ca63546",
+    "url": "/Git-nasrallha/hotelApp.git/static/js/main.13de0a13.chunk.js"
+  },
+  {
+    "revision": "5b53455ce41b33a76439",
+    "url": "/Git-nasrallha/hotelApp.git/static/js/runtime-main.1e3e45bb.js"
+  },
+  {
+    "revision": "fd62c428cccc988d7490496b2ad8ce39",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/aboutbg.fd62c428.jpg"
+  },
+  {
+    "revision": "c1129dc404fcf8852795aa4f280da687",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/cleaner.c1129dc4.jpg"
+  },
+  {
+    "revision": "b7f03a18f74439ae951969f3cf5705f4",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/contactbg.b7f03a18.jpg"
+  },
+  {
+    "revision": "9cf1872682db4f7480e4b1260ab6567b",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/contactus.9cf18726.jpg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/room-9.7b17c49f.jpeg"
+  },
+  {
+    "revision": "cbba3a840feb333a1a8caf54b460dd75",
+    "url": "/Git-nasrallha/hotelApp.git/static/media/service-bg.cbba3a84.jpg"
+  }
+]);
